f3f723f7e9490d1d59de9413d1ed7538a0006144
sonbon2000 <sonbon1101@gmail.com>
2024-07-04 22:51:16 +0700

    implement liked songs feature and play songs function

---
+++ components/PlayerContent.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Song } from "@/types";
+
+interface PlayerContentProps {
+  song: Song;
+  songUrl: string;
+}
+
+export const PlayerContent: React.FC<PlayerContentProps> = ({
+  song,
+  songUrl,
+}) => {
+  return <div>PlayerContent</div>;
+};
